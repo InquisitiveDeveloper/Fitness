d63035a5c938567bc72ab1663e12d452527afbf9
PranayManchala <jokarzy27@gmail.com>
2021-11-06 13:51:45 -0500

    Routingchanges

--- src/js/appbar.js
+++ src/js/appbar.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 import { Grid, Row, Col } from "react-flexbox-grid";
+import { NavLink } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,22 +28,41 @@ const AppBarButton = () => {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-               <Row center="xs">
-
-          <Col xs={6}>ULTIMATE FITNESS</Col>
-        </Row>
-            
+            <Row center="xs">
+              <Col xs={6}>ULTIMATE FITNESS</Col>
+            </Row>
           </Typography>
-          <Button color="inherit"><Link to = "/SignIn"> SignIn</Link></Button>
-          <Button color="inherit"> <Link to = "/SignIn">SignUp</Link></Button>
+          <Button color="inherit"
+          variant = "contained">
+          <NavLink
+              to="/signin"
+              exact
+            >
+              SignIn
+            </NavLink>
+          </Button>
+          <Button color="sucess"
+          variant = "contained">
+            <NavLink
+              to="/signup"
+              exact
+            >
+              SignUp
+            </NavLink>
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
   );
-}
+};
 
 export default AppBarButton;
